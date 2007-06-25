00374ce22e430a4d8c80753c640f214d24159914
AJ Rossini <anthony.rossini@novartis.com>
2007-06-25 08:53:15 +0200

    ls-user has all the exported LispStat functionality.  Make sure to put it in here when usable features are created.

--- ls-user.lisp
+++ ls-user.lisp
@@ -6,6 +6,56 @@
 
 (in-package :cl-user)
 
+
+(defpackage :lisp-stat
+  (:documentation "Experimentation package for LispStat.  Serious work
+should be packaged up elsewhere for reproducibility.")
+  (:nicknames :ls-user)
+  (:use :common-lisp
+	:lisp-stat-object-system
+        :lisp-stat-float
+        :lisp-stat-math
+	:lisp-stat-basics
+	:lisp-stat-regression-linear)
+  (:shadowing-import-from :lisp-stat-object-system
+			  slot-value call-method call-next-method)
+  (:export
+   ;; lsobjects.lsp
+
+   ;; lsmath.lsp
+   ^ ** expt + - * / mod rem pmin pmax abs 1+ 1- log exp sqrt sin cos 
+   tan asin acos atan sinh cosh tanh asinh acosh atanh float random
+   truncate floor ceiling round minusp zerop plusp evenp oddp < <= =
+   /= >= > complex conjugate realpart imagpart phase min max
+   logand logior logxor lognot ffloor fceiling ftruncate fround
+   signum cis
+
+   ;; statistics.lsp
+   open-file-dialog read-data-file read-data-columns load-data 
+   load-example *variables* *ask-on-redefine* def variables savevar
+   undef standard-deviation quantile median interquartile-range
+   fivnum covariance-matrix difference rseq matrix print-matrix solve
+   backsolve eigenvalues eigenvectors accumulate cumsum combine
+   lowess
+
+   ;; regression.lsp
+   regression-model regression-model-proto x y intercept sweep-matrix
+   basis weights included total-sum-of-squares residual-sum-of-squares
+   predictor-names response-name case-labels
+
+   ;; nonlin.lsp
+   nreg-model nreg-model-proto mean-function theta-hat epsilon
+   count-limit verbose
+
+   ;; maximize.lsp
+   newtonmax nelmeadmax
+
+   ;; bayes.lsp
+   bayes-model bayes-model-proto bayes-internals
+
+   ))
+
+
 (defpackage :lisp-stat-user
   (:documentation "Experimentation package for LispStat.  Serious work
 should be packaged up elsewhere for reproducibility.")
@@ -17,3 +67,4 @@ should be packaged up elsewhere for reproducibility.")
 
 (in-package :lisp-stat-user)
 
+;;; 
