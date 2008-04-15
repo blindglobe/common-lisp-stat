bc12443ac8be90cd774419880491e4c12b2d2f67
tony <tony@jeeves.blindglobe.net>
2008-04-15 16:22:32 +0200

    moved test code to end, to own package but same file.  More notes and details.

--- data-clos.lisp
+++ data-clos.lisp
@@ -25,6 +25,7 @@
 ;;; vectors,for example.  "column" vectors are related observations
 ;;; (by measure/recording) while "row" vectors are related readings
 ;;; (by case)
+;;;
 
 (in-package :cl-user)
 
@@ -32,21 +33,21 @@
   (:use :common-lisp
 	:clem )
   (:export dataset ;; primary tool/structure
+
 	   modifyData ;; metadata mods
 	   importData ;; get it in
 	   reshapeData  ;; data mods
 
-	   varNames caseNames
-	   ))
+	   varNames caseNames))
 
 (in-package :lisp-stat-data-clos)
-
 
 ;; Need to figure out typed vectors.   We then map a series of typed
 ;; vectors over to tables where columns are equal typed.  In a sense,
 ;; this is a relation (1-1) of equal-typed arrays.  For the most part,
 ;; this ends up making the R data.frame into a relational building
 ;; block (considering 1-1 mappings using row ID as a relation).  
+;; Is this a worthwhile generalization?
 
 
 (defclass table ())
@@ -91,34 +92,6 @@ Ensure that dims of stored data are same as case and var labels."
        (list (length (var-labels ds))
 	     (length (case-labels ds)))))
 
-(defvar my-ds-2 nil 
-  "test ds for experiment.")
-(setf my-ds-2 (make-instance 'dataset
-			     :storage #2A((1 2 3 4 5) (10 20 30 40 50))
-			     :doc "This is an interesting dataset"
-			     :case-labels (list "a" "b" "c" "d" "e")
-			     :var-labels (list "x" "y")))
-
-(consistent-dataset-p my-ds-2)
-my-ds-2
-(make-array (list 3 5))
-
-(slot-value my-ds-2 'store)
-(dataset my-ds-2)
-(eq (dataset my-ds-2) (slot-value my-ds-2 'store))
-
-(doc-string my-ds-2)
-(case-labels my-ds-2)
-(var-labels my-ds-2)
-;; need to ensure that for things like the following, that we protect
-;; this a bit more so that the results are not going to to be wrong.
-;; That would be a bit nasty if the dataset becomes inconsistent.
-(setf (var-labels my-ds-2) (list "a" "b"))
-(setf (var-labels my-ds-2) (list "a" "b" "c")) ;; Should error!
-(consistent-dataset-p my-ds-2) ;; F
-(setf (var-labels my-ds-2) (list "a" "b"))
-(consistent-dataset-p my-ds-2) ;; T
-
 
 (defun print-structure-table (ds)
   "example of what we want the methods to look like.  Should be sort
@@ -141,10 +114,19 @@ structure."
 
 
 
-(defgeneric extract  (dataform what into-form))
+(defgeneric reshapeData  (dataform into-form as-copy)
+  (:documentation "pulling data into a new form"))
 
-(defmethod extract ((ds dataset) what into-form)
+(defmethod reshapeData ((ds dataset) what into-form)
   (reshape (get ds what) into-form))
+
+
+(defmethod reshapeData ((ds array) (sp specList) copy-p)
+  "Array via specList specialization: similar to the common R
+approaches to redistribution."
+  (let ((widep (getf specList :toWide))
+	(primaryKey (getf specList :primaryKey)))
+    ))
 
 
 
@@ -170,7 +152,7 @@ structure."
 ;;; -    make sure that we do coherency checking in the exported
 ;;; -    functions.
 ;;; -    ... 
-;;; - extract/reformat/reshapr a reformed version of the dataset (no
+;;; - reshapeData/reformat/reshapr a reformed version of the dataset (no
 ;;;           additional input). 
 ;;; -         either overwriting or not, i.e. with or without copy.
 ;;; - check consistency of resulting data with metadata and related
@@ -189,9 +171,6 @@ structure."
 
 (defsetf varNames set-varNames)
 
-(varNames my-ds-2)
-(setf (varNames my-ds-2) (list "a" "b"))
-(varNames my-ds-2)
 
 
 ;;; Case-name handling for Tables.  Needs error checking.
@@ -206,41 +185,110 @@ structure."
 
 (defsetf caseNames set-caseNames)
 
-(defvar origCaseNames nil)
-(setf origCaseNames (caseNames my-ds-2))
-(setf (caseNames my-ds-2) (list "a" "b" "c" 4 5))
-(caseNames my-ds-2)
-(setf (caseNames my-ds-2) (list "a" "b" 4 5))
-(setf (caseNames my-ds-2) origCaseNames)
-
 
 (defgeneric importData (source featureList)
   "command to get data into CLS.  Specific methods will need to handle
-  files, internal data structures, and DBMS's.")
+  files, internal data structures, and DBMS's.  We would like to be
+  able to do:
 
-(defmethod importData ((fileHandle pathfile)
+
+
+")
+
+
+(defun pathname-example (name) 
+  (let ((path (parse-namestring name)))) 
+    (values (pathname-name path :case :common) 
+            (pathname-name path :case :local)))) 
+
+(defvar sourceTypes (list 'csv 'lisp 'tsv 'special)
+  "list of possible symbols used to specify source formats that might
+be supported for input.  CSV and TSV are standard, LISP refers to
+forms, and SPECIAL refers to a FUNCTION which parses as
+appropriately.") 
+
+;;; WRONG LOGIC.
+(defmethod importData ((fileHandle pathname)
 		       (fmt sourceTypes))
-  "File-based input for data."
-  (let ((newData (getData fileHandle 'csv)))
-    (progn
-      (if (colnames-in fmt)
-	  (setf (varNames newData) (var 1 newData))
-	  (setf (varNames newData) nil))
-      (if (rownames-in fmt)
-	  (setf (caseNames newData) (var 1 newData))
-	  (setf (caseNames newData) nil)))))
+  "File-based input for data.
+Usually used by:
+ (importData (parse-namestring 'path/to/file')
+	     (list :format 'csv))
 
-      
+ (importData myPathName (list :format 'lisp))
+."
+  (let ((newData (getDataAsLists fileHandle fmtType))
+	(fmtType (getf fmt :format)))
+    (case fmtType
+      ('csv (  ))
+      ('tsv (  ))
+      ('lisp ( ))
+      ('special (let ((parserFcn (getf fmt :special-parser)))))
+      (:default (error "no standard default importData format")))))
+
 (defmethod importData ((ds array) (fmt list))
   "mapping arrays into CLS data.")
-
 
 (defmethod importData ((dsSpec DBMSandSQLextract)
 		       (fmt mappingTypes))
   "mapping DBMS into CLS data.")
 
 
-(defgeneric reshapeData (ds spec copy-p)
-  "Subset, augment, reshape a table into a table.")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; EXPERIMENT
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package :cl-user)
+
+(defpackage :lisp-stat-data-clos-example
+  (:use :common-lisp
+	:clem
+	:lisp-stat-data-clos))
+
+(in-package :lisp-stat-data-clos-example)
 
 
+(defvar my-ds-2 nil 
+  "test ds for experiment.")
+(setf my-ds-2 (make-instance 'dataset
+			     :storage #2A((1 2 3 4 5) (10 20 30 40 50))
+			     :doc "This is an interesting dataset"
+			     :case-labels (list "a" "b" "c" "d" "e")
+			     :var-labels (list "x" "y")))
+
+(consistent-dataset-p my-ds-2)
+my-ds-2
+(make-array (list 3 5))
+
+(slot-value my-ds-2 'store)
+(dataset my-ds-2)
+(eq (dataset my-ds-2) (slot-value my-ds-2 'store))
+
+(doc-string my-ds-2)
+(case-labels my-ds-2)
+(var-labels my-ds-2)
+
+
+;; need to ensure that for things like the following, that we protect
+;; this a bit more so that the results are not going to to be wrong.
+;; That would be a bit nasty if the dataset becomes inconsistent.
+(setf (var-labels my-ds-2) (list "a" "b"))
+(setf (var-labels my-ds-2) (list "a" "b" "c")) ;; Should error!
+(consistent-dataset-p my-ds-2) ;; F
+(setf (var-labels my-ds-2) (list "a" "b"))
+(consistent-dataset-p my-ds-2) ;; T
+
+;; This is now done by:
+	
+(varNames my-ds-2)
+(setf (varNames my-ds-2) (list "a" "b"))
+(varNames my-ds-2)
+
+
+(defvar origCaseNames nil)
+(setf origCaseNames (caseNames my-ds-2))
+(setf (caseNames my-ds-2) (list "a" "b" "c" 4 5))
+(caseNames my-ds-2)
+(setf (caseNames my-ds-2) (list "a" "b" 4 5))
+(setf (caseNames my-ds-2) origCaseNames)
