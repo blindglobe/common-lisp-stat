a4251c10c921630ddb35caf8040327aa175f9b1d
tony <tony@jeeves.blindglobe.net>
2008-05-10 08:42:12 +0200

    removed dummy file.

--- unittests-dataclos.lisp
+++
@@ -1,6 +0,0 @@
-;;; -*- mode: lisp -*-
-;;; Copyright (c) 2007, by A.J. Rossini <blindglobe@gmail.com>
-;;; See COPYRIGHT file for any additional restrictions (BSD license).
-;;; Since 1991, ANSI was finally finished.  Edited for ANSI Common Lisp. 
-
-b 
