3a75310f7477251c9ece80f6372c6f4954e8f124
tony <tony@jeeves.blindglobe.net>
2008-05-20 08:18:42 +0200

    why do we need to load lift?  Why is it special?

--- ls-demo.lisp
+++ ls-demo.lisp
@@ -3,7 +3,7 @@
 ;;; See COPYRIGHT file for any additional restrictions (BSD license).
 ;;; Since 1991, ANSI was finally finished.  Edited for ANSI Common Lisp. 
 
-;;; Time-stamp: <2008-05-17 09:37:01 tony>
+;;; Time-stamp: <2008-05-19 08:38:13 tony>
 ;;; Creation:   <2007-01-01 09:21:50 user> WRONG
 ;;; File:       ls-demo.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
@@ -15,6 +15,7 @@
 ;;; designers and quality assurance people in its wake.
 
 (in-package :cl-user)
+(asdf:oos 'asdf:load-op 'lift) ;; we need this, but I don't know why?
 (asdf:oos 'asdf:load-op 'lispstat)
 
 ;;; non-rigorous check for exports.
