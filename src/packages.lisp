a523810ae6a40a746dcec998c6a01f9094bbae1c
AJ Rossini <blindglobe@gmail.com>
2008-09-12 08:11:23 +0200

    initial start of moving towards "one-stop" understanding of what gets exported out of each package, and what the design looks like.

---
+++ src/packages.lisp
@@ -0,0 +1,74 @@
+;;; -*- mode: lisp -*-
+
+;;; Time-stamp: <2008-09-08 08:28:47 tony>
+;;; Creation:   <2008-03-11 19:18:34 user> 
+;;; File:       packages.lisp
+;;; Author:     AJ Rossini <blindglobe@gmail.com>
+;;; Copyright:  (c)2007--2008, AJ Rossini.  BSD, LLGPL, or GPLv2, depending
+;;;             on how it arrives.  
+;;; Purpose:    package structure description for lispstat
+
+;;; What is this talk of 'release'? Klingons do not make software
+;;; 'releases'.  Our software 'escapes', leaving a bloody trail of
+;;; designers and quality assurance people in its wake.
+
+;;; This organization and structure is new to the 21st Century
+;;; version.
+
+(in-package :cl-user)
+
+
+
+
+;;; REGRESSION using CLOS structure 
+
+(defpackage :lisp-stat-regression-linear-clos
+  (:use :common-lisp
+	:clem )
+
+  (:export regression-model regression-model-obj x y intercept sweep-matrix
+	   basis weights included total-sum-of-squares residual-sum-of-squares
+	   predictor-names response-name case-labels))
+
+;;; LispStat Basics
+
+(defpackage :lisp-stat-basics
+    (:use :common-lisp
+	  :lisp-stat-object-system
+	  :lisp-stat-types
+	  :lisp-stat-float
+	  :lisp-stat-macros
+	  :lisp-stat-compound-data)
+  (:shadowing-import-from :lisp-stat-object-system
+			  slot-value call-method call-next-method)
+  (:export permute-array sum prod count-elements mean
+	   if-else sample))
+
+
+;;;
+
+
+
+(defpackage :lisp-stat-float
+  (:use :common-lisp)
+  (:export +stat-float-typing+ +stat-cfloat-typing+ +stat-float-template+
+	   machine-epsilon base-float makedouble
+
+	   make-base-trans-fun-2 make-base-trans-fun 
+
+	   BASE-LOG BASE-EXP BASE-EXPT BASE-SQRT BASE-SIN BASE-COS
+	   BASE-TAN BASE-ASIN BASE-ACOS BASE-ATAN BASE-SINH
+	   BASE-COSH BASE-TANH BASE-ASINH BASE-ACOSH BASE-ATANH
+	   BASE-ABS BASE-PHASE BASE-FFLOOR BASE-FCEILING BASE-FTRUNCATE
+	   BASE-FROUND BASE-SIGNUM BASE-CIS))
+
+;;; 
+
+
+
+(defpackage :lisp-stat-macros
+  (:use :common-lisp
+	:lisp-stat-compound-data)
+  (:export make-rv-function make-rv-function-1))
+
+;;; 
