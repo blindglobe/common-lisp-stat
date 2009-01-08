4758fbd0b0b36b2a0737759feeeded8310a5e0d1
AJ Rossini <blindglobe@gmail.com>
2009-01-08 08:33:36 +0100

    more philosophy, without code

--- TODO.lisp
+++ TODO.lisp
@@ -1,6 +1,6 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2009-01-03 19:36:11 tony>
+;;; Time-stamp: <2009-01-08 08:25:03 tony>
 ;;; Creation:   <2008-09-08 08:06:30 tony>
 ;;; File:       TODO.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
@@ -195,4 +195,174 @@
 
   ;; but I think the cl-csv package is broken, need to use the dsv-style
   ;; package.
+
+  ;; now we've got the DSV code in the codebase, auto-loaded I hope:
+  cybertiggyr-dsv:*field-separator*
+  (defparameter *example-numeric.csv* 
+    (cybertiggyr-dsv:load-escaped "Data/example-numeric.csv"
+				  :field-separator #\,))
+  *example-numeric.csv*
+
+  ;; the following fails because we've got a bit of string conversion
+  ;; to do.   2 thoughts: #1 modify dsv package, but mucking with
+  ;; encapsulation.  #2 add a coercion tool (better, but potentially
+  ;; inefficient).
+  #+nil(coerce  (nth 3 (nth 3 *example-numeric.csv*)) 'double-float)
+
+  ;; cases, simple to not so
+  (defparameter *test-string1* "1.2")
+  (defparameter *test-string2* " 1.2")
+  (defparameter *test-string3* " 1.2 ")
+
+  
+  )
+
+
+#+nil
+(progn ;; experiments with GSL and the Lisp interface.
+  (asdf:oos 'asdf:load-op 'gsll)
+  (asdf:oos 'asdf:load-op 'gsll-tests)
+
+  ;; the following should be equivalent
+  (setf *t1*  (LIST 6.18d0 6.647777777777779d0 6.18d0))
+  (setf *t2*  (MULTIPLE-VALUE-LIST
+	       (LET ((VEC
+		      (gsll:make-marray 'DOUBLE-FLOAT
+					:INITIAL-CONTENTS '(-3.21d0 1.0d0 12.8d0)))
+		     (WEIGHTS
+		      (gsll:MAKE-MARRAY 'DOUBLE-FLOAT
+					:INITIAL-CONTENTS '(3.0d0 1.0d0 2.0d0))))
+		 (LET ((MEAN (gsll:MEAN VEC)))
+		   (LIST (gsll:ABSOLUTE-DEVIATION VEC)
+			 (gsll:WEIGHTED-ABSOLUTE-DEVIATION VEC WEIGHTS)
+			 (gsll:ABSOLUTE-DEVIATION VEC MEAN))))))
+  (eql *t1* *t2*)
+
+  ;; from (gsll:examples 'gsll::numerical-integration) ...
+  (gsll:integration-qng gsll::one-sine 0.0d0 PI)
+
+
+  (defun-single axpb (x) (+ (* 2 x) 3)) ;; a<-2, b<-3
+  (gsll:integration-qng axpb 1d0 2d0)
+
+  (let ((a 2)
+	(b 3))
+    (defun-single axpb2 (x) (+ (* a x) b)))
+  (gsll:integration-qng axpb2 1d0 2d0)
+
+
+#| BAD
+  (gsll:integration-qng 
+   (let ((a 2)
+	 (b 3))
+     (defun-single axpb2 (x) (+ (* a x) b)))
+   1d0 2d0)
+|#
+
+  ;; right, but weird expansion...
+  (gsll:integration-qng 
+   (let ((a 2)
+	 (b 3))
+     (defun axpb2 (x) (+ (* a x) b))
+     (def-single-function axpb2)
+     axpb2)
+   1d0 2d0)
+
+
+  )
+
+
+
+#+nil
+(progn ;; philosophy time
+  
+  (setf my-model (model :name "ex1"
+			:data-slots (list x y z)
+			:param-slots (list alpha beta gamma)
+			:math-form (regression-model :formula '(= y (+ (* beta x)
+								     (* alpha y)
+								     (* gamma z)
+								     normal-error)))))
+  (setf my-dataset (statistical-table :table data-frame-contents
+				      :metadata (list (:case-names (list ))
+						      (:var-names (list ))
+						      (:documentation "string of doc"))))
+
+  (setf my-analysis (analysis
+		     :model my-model
+		     :data my-dataset
+		     :parameter-map (pairing (model-param-slots my-model)
+					     (data-var-names my-dataset))))
+
+  ;; ontological implications -- the analysis is an abstract class of
+  ;; data, model, and mapping between the model and data.  The fit is
+  ;; the instantiation of such.  This provides a statistical object
+  ;; computation theory which can be realized as "executable
+  ;; statistics" or "computable statistics".
+  (setf my-analysis (analyze my-fit
+			     :estimation-method 'linear-least-squares-regression))
+
+  ;; one of the tricks here is that one needs to provide the structure
+  ;; from which to consider estimation, and more importantly, the
+  ;; validity of the estimation.
+
+  ;;
+  (setf linear-least-squares-regression
+	(estimation-method-definition
+	 :variable-defintions ((list 
+				;; from MachLearn: supervised,
+				;; unsupervised
+				:data-response-vars list-drv ; nil if unsup
+				;; 
+				:param-vars list-pv
+				     :data-predictor-vars list-dpv 
+				     ;; nil in this case.  these
+				     ;; describe "out-of-box" specs
+				     :hyper-vars list-hv))
+	 :form '(regression-additive-error
+		 :central-form (linear-form drv pv dpv)
+		 :error-form 'normal-error)
+	 :resulting-decision '(point-estimation interval-estimation)
+	 :philosophy 'frequentist
+	 :documentation "use least squares to fit a linear regression
+                         model to data."))
+
+  (defparameter *statistical-philosophies*
+    '(frequentist bayesian fiducial decision-analysis)
+    "can be combined to build decision-making approaches and
+    characterizations")
+
+  (defparameter *decisions*
+    '(estimation selection testing)
+    "possible results from a...")
+  ;; is this really true?  One can embedded hypothesis testing within
+  ;; estimation, as the hypothesis estimated to select.  And
+  ;; categorical/continuous rear their ugly heads, but not really in
+  ;; an essential way.
+
+  (defparameter *ontology-of-decision-procedures*
+    (list :decisions
+	  (list :estimation
+		(list :point
+		      (list :maximum-likelihood
+			    :minimum-entropy
+			    :least-squares
+			    :method-of-moments)
+		      :interval
+		      (list :maximum-likelihood
+			    :))
+		:testing
+		(list :fisherian
+		      :neyman-pearson
+		      (list :traditional
+			    :bioequivalence-inversion)
+		      :selection
+		      (list :ranking
+			    :top-k-of-n-select))
+		:parametric
+		:partially-parametric))
+    "start of ontology")
+
+
+
   )
