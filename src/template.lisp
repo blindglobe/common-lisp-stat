88cfee49adbb302040e420643b9765d85372fed9
AJ Rossini <blindglobe@gmail.com>
2009-03-16 14:55:09 +0100

    template for code.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

---
+++ src/template.lisp
@@ -0,0 +1,17 @@
+;;; -*- mode: lisp -*-
+
+;;; Time-stamp: <2009-03-12 17:15:10 tony>
+;;; Creation:   <2009-03-12 17:14:56 tony>
+;;; File:       template.lisp
+;;; Author:     AJ Rossini <blindglobe@gmail.com>
+;;; Copyright:  (c)2009--, AJ Rossini.  BSD, LLGPL, or GPLv2, depending
+;;;             on how it arrives.  
+;;; Purpose:    Template header file
+
+;;; What is this talk of 'release'? Klingons do not make software
+;;; 'releases'.  Our software 'escapes', leaving a bloody trail of
+;;; designers and quality assurance people in its wake.
+
+;;; This organization and structure is new to the 21st Century
+;;; version.
+
