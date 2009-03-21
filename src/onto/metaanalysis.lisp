03cf9048c472538725527fa2851a5c9a3a43322d
AJ Rossini <blindglobe@gmail.com>
2009-03-21 09:38:26 +0100

    onto entry for metaanalysis, needs review and correction.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

---
+++ src/onto/metaanalysis.lisp
@@ -0,0 +1,38 @@
+
+(session
+ :title "role of metaanalysis in "
+
+ (talk
+  :name "Gordon Lan"
+  :title "Use of cumulative meta-analysis"
+  :topic "treatment effect"
+
+  (review "mixed-effects")
+
+  (concept "accumulating a distribution over trials"
+	   "use approximation to normality (discr -> contin)"
+	   
+	   "similar to group-seq, but diff ; involves open seq testing"
+	   "if max info fixed in advance, transform to group-seq problem" 
+	   ""
+	   )
+
+  
+  )
+
+ (talk
+  :name ""
+  :title ""
+  :topic "")
+
+ (talk
+  :name ""
+  :title ""
+  :topic "")
+
+ (talk
+  :name ""
+  :title ""
+  :topic "")
+
+	 )
