78dec2cca96e0604b7a727f624257f10f541048f
AJ Rossini <blindglobe@gmail.com>
2009-03-23 17:51:04 +0100

    nearly nothing from my discussion with hadley

---
+++ src/algorithms/tour.lisp
@@ -0,0 +1,18 @@
+
+
+(defmacro tour (())
+  "
+
+ (do-tour ((:projection p)
+           (:interpolation i))
+      (forms providing display d))
+
+"
+  )
+
+(defclass projection ()
+  )
+
+(defclass interpolate ()
+  )
+
