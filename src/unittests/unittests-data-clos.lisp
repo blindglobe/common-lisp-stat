3aa2d8472ad2e0b665f1699ac0a07cb2f73686b6
AJ Rossini <blindglobe@gmail.com>
2009-03-31 17:06:18 +0200

    more work on the dataframe unittests.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- src/unittests/unittests-data-clos.lisp
+++ src/unittests/unittests-data-clos.lisp
@@ -5,7 +5,7 @@
 ;;; Copyright:  (c)2008, AJ Rossini.  BSD, LLGPL, or GPLv2, depending
 ;;;             on how it arrives.  
 ;;; Purpose:    unittests for the data-clos package
-;;; Time-stamp: <2009-03-25 08:02:27 tony>
+;;; Time-stamp: <2009-03-31 08:30:13 tony>
 ;;; Creation:   <2008-05-09 14:18:19 tony>
 
 ;;; What is this talk of 'release'? Klingons do not make software
@@ -16,7 +16,7 @@
 
 (deftestsuite lisp-stat-ut-dataclos (lisp-stat-ut) ())
 
-;; Ensure helper-function support
+;;; Ensure helper-functions work
 
 (addtest (lisp-stat-ut-dataclos) genseq
 	 (ensure
@@ -33,78 +33,100 @@
 	  (equal (lisp-stat-data-clos::gen-seq 4 2)
 		 (list 2 3 4))))
 
-(addtest (lisp-stat-ut-dataclos) equaltestnameData
-	 (ensure-error
-	  (equal (lisp-stat-data-clos::dataset
-		  (make-instance 'dataframe-array
-				 :storage #2A(('a 'b)
-					      ('c 'd))))
-		 #2A(('a 'b)
-		     ('c 'd)))))
 
-#|
- (defvar my-ds-1 nil 
-  "test ds for experiment.")
- (setf my-ds-1 (make-instance 'dataframe-array))
- my-ds-1
-|#
-
-(defvar my-ds-2 nil 
-  "test ds for experiment.")
-
-(setf my-ds-2 (make-instance 'dataframe-array
-			     :storage #2A((1 2 3 4 5)
-					  (10 20 30 40 50))
-			     :doc "This is an interesting dataframe-array"
-			     :case-labels (list "a" "b" "c" "d" "e")
-			     :var-labels (list "x" "y")))
-my-ds-2
-(make-array (list 3 5))
-
-(array-dimensions (lisp-stat-data-clos::dataset my-ds-2))
-
-
-(addtest (lisp-stat-ut-dataclos) consData
+(addtest (lisp-stat-ut-dataclos) repeatseq
 	 (ensure
-	  (consistent-dataframe-like-p my-ds-2)))
+	  (equal (lisp-stat-data-clos::repeat-seq 3 "d")
+		 (list "d" "d" "d")))
+	 	 (ensure
+	  (equal (lisp-stat-data-clos::repeat-seq 3 'd)
+		 (list 'd 'd 'd))))
 
-(addtest (lisp-stat-ut-dataclos) badAccess1
-	 (ensure-error
-	  (slot-value my-ds-2 'store)))
 
-(addtest (lisp-stat-ut-dataclos) badAccess2
-	 (ensure-error
-	  (slot-value my-ds-2 'store)))
 
-(addtest (lisp-stat-ut-dataclos) badAccess3
-	 (ensure-error
-	  (lisp-stat-data-clos::dataset my-ds-2)))
 
-(addtest (lisp-stat-ut-dataclos) badAccess4
+
+
+
+
+
+
+;;; Dataframe
+
+(addtest (lisp-stat-ut-dataclos) df-equalp
 	 (ensure
-	  (equal
-	   (slot-value my-ds-2 'lisp-stat-data-clos::store)
-	   (lisp-stat-data-clos::dataset my-ds-2))))
+	  (equalp (dataset (make-instance 'dataframe-array
+					  :storage #2A(('a 'b)
+						       ('c 'd))))
+		  #2A(('a 'b)
+		      ('c 'd)))))
 
+
+;; FAKE DF
+(defparameter *my-df-0*
+  (make-instance 'dataframe-array
+		 :storage #2A((1 2 3 4 5)
+			      (10 20 30 40 50))
+		 :doc "This is an interesting dataframe-array"
+		 :case-labels (list "a" "b" "c" "d" "e")
+		 :var-labels (list "x" "y")))
+
+;; WORKING DF
+(defparameter *my-df-1*
+  (make-instance 'dataframe-array
+		 :storage #2A((1 2 3 4 5)
+			      (10 20 30 40 50))
+		 :doc "This is an interesting dataframe-array"
+		 :case-labels (list "x" "y")
+		 :var-labels  (list "a" "b" "c" "d" "e")))
+
+(addtest (lisp-stat-ut-dataclos) df-consdata
+	 (ensure
+	  (consistent-dataframe-p *my-df-1*)))
+
+(addtest (lisp-stat-ut-dataclos) df-access-1
+	 (ensure-error
+	  (slot-value *my-df-1* 'store)))
+
+(addtest (lisp-stat-ut-dataclos) df-access-2
+	 (ensure-error
+	  (slot-value *my-df-1* 'store)))
+
+(addtest (lisp-stat-ut-dataclos) df-access-3
+	 (ensure
+	  (dataset *my-df-1*)))
+
+(addtest (lisp-stat-ut-dataclos) df-access-4
+	 (ensure
+	  (equalp
+	   (slot-value *my-df-1* 'lisp-stat-data-clos::store)
+	   (lisp-stat-data-clos::dataset *my-df-1*))))
 
 (addtest (lisp-stat-ut-dataclos) badAccess5
 	 (ensure
-	  (eq (lisp-stat-data-clos::dataset my-ds-2)
-	      (slot-value my-ds-2 'lisp-stat-data-clos::store))))
+	  (eq (lisp-stat-data-clos::dataset *my-df-1*)
+	      (slot-value *my-df-1* 'lisp-stat-data-clos::store))))
 
 
-;; NEVER DO THE FOLLOWING, UNLESS YOU WANT TO MUCK UP STRUCTURES...
+;; NEVER REACH INTO CLASS INTERIORS, NO PROMISE GUARANTEE OF LATER CONSISTENCY... 
+
 (addtest (lisp-stat-ut-dataclos) badAccess6
 	 (ensure
-	  (lisp-stat-data-clos::doc-string my-ds-2)))
+	  (lisp-stat-data-clos::doc-string *my-df-1*))
+	 (ensure-error 
+	  (doc-string *my-df-1*)))
 
 (addtest (lisp-stat-ut-dataclos) badAccess7
 	 (ensure
-	  (lisp-stat-data-clos::case-labels my-ds-2)))
+	  (lisp-stat-data-clos::case-labels *my-df-1*))
+	 (ensure-error
+	  (case-labels *my-df-1*)))
 
 (addtest (lisp-stat-ut-dataclos) badAccess8
 	 (ensure
-	  (lisp-stat-data-clos::var-labels my-ds-2)))
+	  (lisp-stat-data-clos::var-labels *my-df-1*))
+	 (ensure-error
+	  (var-labels *my-df-1*)))
 
 ;; need to ensure that for things like the following, that we protect
 ;; this a bit more so that the results are not going to to be wrong.
@@ -113,33 +135,33 @@ my-ds-2
 
 (addtest (lisp-stat-ut-dataclos) badAccess9
 	 (ensure
-	  (setf (lisp-stat-data-clos::var-labels my-ds-2)
+	  (setf (lisp-stat-data-clos::var-labels *my-df-1*)
 		(list "a" "b"))))
 
 (addtest (lisp-stat-ut-dataclos) badAccess10
 	 (ensure
 	  (progn 
 	    ;; no error, but corrupts structure
-	    (setf (lisp-stat-data-clos::var-labels my-ds-2)
+	    (setf (lisp-stat-data-clos::var-labels *my-df-1*)
 		  (list "a" "b" "c"))
 	    ;; error happens here
-	    (not (consistent-dataframe-like-p my-ds-2))))) ;; Nil
+	    (not (consistent-dataframe-like-p *my-df-1*))))) ;; Nil
 
 (addtest (lisp-stat-ut-dataclos) badAccess12
 	 (ensure
-	  (setf (lisp-stat-data-clos::var-labels my-ds-2)
+	  (setf (lisp-stat-data-clos::var-labels *my-df-1*)
 		(list "a" "b"))))
 
 (addtest (lisp-stat-ut-dataclos) badAccess13
 	 (ensure
-	  (consistent-dataframe-like-p my-ds-2))) ;; T
+	  (consistent-dataframe-like-p *my-df-1*))) ;; T
 
 ;; This is now done by:
 (addtest (lisp-stat-ut-dataclos) badAccess14
 	 (ensure-error
-	  (let ((old-varnames (varNames my-ds-2)))
-	    (setf (varNames my-ds-2) (list "a" "b")) ;; should error
-	    (setf (varNames my-ds-2) old-varnames)
+	  (let ((old-varnames (varNames *my-df-1*)))
+	    (setf (varNames *my-df-1*) (list "a" "b")) ;; should error
+	    (setf (varNames *my-df-1*) old-varnames)
 	    (error "don't reach this point in badaccess14"))))
 
 ;; break this up.
@@ -148,13 +170,13 @@ my-ds-2
 (addtest (lisp-stat-ut-dataclos) badAccess15
 	 (ensure
 	  (progn
-	    (setf origCaseNames (caseNames my-ds-2))
-	    (setf (caseNames my-ds-2) (list "a" "b" "c" 4 5))
-	    (caseNames my-ds-2)
+	    (setf origCaseNames (caseNames *my-df-1*))
+	    (setf (caseNames *my-df-1*) (list "a" "b" "c" 4 5))
+	    (caseNames *my-df-1*)
 	    (ignore-errors
-	      (setf (caseNames my-ds-2)
+	      (setf (caseNames *my-df-1*)
 		    (list "a" "b" 4 5)))
-	    (setf (caseNames my-ds-2) origCaseNames))))
+	    (setf (caseNames *my-df-1*) origCaseNames))))
 
 ;;;
 ;; (run-tests)
