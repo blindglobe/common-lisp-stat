5536bc0256d58f2c534cd09f7ec82b605e39e63a
AJ Rossini <blindglobe@gmail.com>
2009-04-20 08:06:33 +0200

    more examples to fill in.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

---
+++ examples/linear-regression.lisp
@@ -0,0 +1,25 @@
+;;; -*- mode: lisp -*-
+
+;;; Time-stamp: <2009-04-19 09:53:09 tony>
+;;; Creation:   <2009-04-19 09:41:09 tony>
+;;; File:       linear-regression.lisp
+;;; Author:     AJ Rossini <blindglobe@gmail.com>
+;;; Copyright:  (c)2009--, AJ Rossini.  BSD, MIT, LLGPL, or
+;;;             GPLv2+, or GPLv3+ depending on how it arrives.  
+;;; Purpose:    Example of basic exploratory data analysis in CLS. 
+
+;;; What is this talk of 'release'? Klingons do not make software
+;;; 'releases'.  Our software 'escapes', leaving a bloody trail of
+;;; designers and quality assurance people in its wake.
+
+
+(in-package :ls-user)
+
+(defparameter *my-df*
+  (make-dataframe
+   (rsm.string::filestream->string-table
+    "/path/to/file.dsv"))
+  "Initial read-in of data.")
+
+
+;; now to build a linear regression model from *my-df*
