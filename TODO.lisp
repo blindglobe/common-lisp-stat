08d9ff6cb5f06c6f42c74ffcdd37a99782a553f6
AJ Rossini <blindglobe@gmail.com>
2009-06-25 09:38:51 +0200

    included example for drawing and capturing via PNG the drawing.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- TODO.lisp
+++ TODO.lisp
@@ -1,6 +1,6 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2009-06-18 18:43:02 tony>
+;;; Time-stamp: <2009-06-24 17:30:03 tony>
 ;;; Creation:   <2008-09-08 08:06:30 tony>
 ;;; File:       TODO.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
@@ -226,11 +226,12 @@
 	 (xs (num-sequence :from 0 :to 10 :length n))
 	 (ys (map 'vector #'(lambda (x) (+ x 8 (random 4.0))) xs))
 	 (weights (replicate #'(lambda () (1+ (random 10))) n 'fixnum))
-	 (da (plot-simple frame (interval-of 0 10) (interval-of 10 20)
+	 (da (plot-simple *frame1* (interval-of 0 10) (interval-of 10 20)
 			  :x-title "x" :y-title "y")))
     (draw-symbols da xs ys :weights weights))
 
-
+  (xlib-image-context-to-png (context *frame1*) "/home/tony/test1.png")
+  (xlib-image-context-to-png (context *frame2*) "/home/tony/test2.png")
   )
 
 
