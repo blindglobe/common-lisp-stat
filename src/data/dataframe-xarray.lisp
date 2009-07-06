14b8275608eeba2dce68e4b3a366cd07e90d4af7
AJ Rossini <blindglobe@gmail.com>
2009-07-06 18:16:08 +0200

    more dataframe-xarray work, not finished.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- src/data/dataframe-xarray.lisp
+++ src/data/dataframe-xarray.lisp
@@ -1,6 +1,6 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2009-06-23 08:02:51 tony>
+;;; Time-stamp: <2009-07-05 02:08:15 tony>
 ;;; Creation:   <2009-06-22 17:09:47 tony>
 ;;; File:       dataframe-xarray.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
@@ -24,16 +24,21 @@
 (defmethod (setf xref) (value (object array) &rest subscripts)
   (setf (apply #'dfref object subscripts) value))
   
-(defmethod xref ((obj matrix-like)  &rest indices))
+(defmethod xref ((obj matrix-like) &rest indices))
   
-(defmethod xtype ((obj dataframe-like)))
+(defmethod xtype ((obj dataframe-like))
+  "Unlike the standard xtype, here we need to return a vector of the types.")
 
-(defmethod xdims ())
+(defmethod xdims ((obj dataframe-like))
+  (dataframe-dimensions obj))
+#|
+ ;; use default methods at this point, except for potentially weird DFs
+ (defmethod xdims* ())
 
-(defmethod xdims* ())
-
-(defmethod xdim ((obj dataframe-like) index)
+ (defmethod xdim ((obj dataframe-like) index)
   (dataframe-dimension index))
+
+|#
 
 (defmethod xrank ())
 
