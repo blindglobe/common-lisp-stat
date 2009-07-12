74e943b8faccd56916b0665a3a06c92f7c4f4da6
AJ Rossini <blindglobe@gmail.com>
2009-07-12 18:20:27 +0200

    clean up errors, introduce a few things to implement.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- examples/linear-regression.lisp
+++ examples/linear-regression.lisp
@@ -1,6 +1,6 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2009-07-11 13:43:05 tony>
+;;; Time-stamp: <2009-07-12 13:20:41 tony>
 ;;; Creation:   <2009-04-19 09:41:09 tony>
 ;;; File:       linear-regression.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
@@ -26,13 +26,13 @@
 
 (defparameter *m2*
   (lm (list->vector-like iron)
-		    (list->vector-like absorbtion))
+      (list->vector-like absorbtion))
   "holding variable.")
 
 (defparameter *m-fit*
   (fit-model *m*))
 
-(princ *m*)
+(princ *m2*)
 (princ *m-fit*)
 
 (estimates *m-fit*)
@@ -56,7 +56,7 @@
 |#
   
 (estimates *m3-fit*)
-(covariance-matrix *m3-fit*))
+(covariance-matrix *m3-fit*)
 
 
 
@@ -64,6 +64,11 @@
 
 (defparameter *my-df*
   (make-dataframe
-   (rsm.string::filestream->string-table
-    "/path/to/file.dsv"))
-  "Initial read-in of data.")
+   (rsm.string::file->string-table
+    (concatenate 'string *lispstat-data-dir*  "file.dsv"))
+  "Initial read-in of data."))
+
+(defparameter *my-resp-var* (slice *my-df* ))
+
+(defparameter *my-pred-vars* (slice *my-df* ))
+
