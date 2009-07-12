47b98ac2906517d48f1f3273ff9be3aaa423e5bc
AJ Rossini <blindglobe@gmail.com>
2009-07-12 08:01:51 +0200

    remind self to consider packaging to avoid namespace pollution.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- examples/read-me.lisp
+++ examples/read-me.lisp
@@ -1,6 +1,6 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2009-07-11 13:48:39 tony>
+;;; Time-stamp: <2009-07-12 07:55:04 tony>
 ;;; Creation:   <2009-03-12 17:14:56 tony>
 ;;; File:       read-me.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
@@ -28,3 +28,6 @@
 
 ;; Be prepared to have a clean-up function (or attach/wrap a cleanup
 ;; hook.
+
+;; Consider the use of a demo or example package to avoid pollution of
+;; the ls-user namespace to prevent the ls-user from becoming a luser.
