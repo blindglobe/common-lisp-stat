1f9108e3fcae86cbddf8590bc258b8c77b29b706
AJ Rossini <blindglobe@gmail.com>
2009-07-18 11:46:33 +0200

    macro problem -- code was out of quote, fixed.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- src/data/data.lisp
+++ src/data/data.lisp
@@ -1,6 +1,6 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2009-07-14 19:32:04 tony> 
+;;; Time-stamp: <2009-07-18 11:44:57 tony> 
 ;;; Creation:   <2005-08-xx 21:34:07 rossini> 
 ;;; File:       data.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
@@ -263,12 +263,13 @@ VAR is not evaluated and must be a symbol.  Assigns the value of FORM to
 VAR and adds VAR to the list *VARIABLES* of def'ed variables. Returns VAR.
 If VAR is already bound and the global variable *ASK-ON-REDEFINE*
 is not nil then you are asked if you want to redefine the variable."
-  `(unless (and *ask-on-redefine*
-		(boundp ',symbol)
-		(not (y-or-n-p "Variable has a value. Redefine?")))
-     (defparameter ,symbol ,value))
-  (pushnew ',symbol *variables*)
-  ',symbol)
+  `(progn
+     (unless (and *ask-on-redefine*
+		  (boundp ',symbol)
+		  (not (y-or-n-p "Variable has a value. Redefine?")))
+       (defparameter ,symbol ,value))
+     (pushnew ',symbol *variables*)
+     ',symbol))
   
 (defun variables-list () 
   "Return list of variables as a lisp list of strings."
