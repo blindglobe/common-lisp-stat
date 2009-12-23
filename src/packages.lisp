9d833958a9397aabe963059881bf2b3adf87a342
AJ Rossini <blindglobe@gmail.com>
2009-12-23 14:36:04 +0100

    remove lisp-stat-config from packages -- it's in the ASDF configurator.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- src/packages.lisp
+++ src/packages.lisp
@@ -1,6 +1,6 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2009-12-23 08:33:58 tony>
+;;; Time-stamp: <2009-12-23 14:31:44 tony>
 ;;; Creation:   <2008-03-11 19:18:34 user> 
 ;;; File:       packages.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
@@ -16,34 +16,20 @@
 
 ;;; Current structure, dependencies:
 #|
- (progn
-   (ls-user :depends-on lisp-stat)
-   (lisp-stat :depends-on '(cls-dataframe
-			    cls-data
-			    lisp-matrix
-			    ))
-   (cls-dataframe :depends-on cls-data)
+ '((:ls-user       :depends-on   lisp-stat)
+   (:lisp-stat     :depends-on '(cls-dataframe
+                                cls-data
+			        lisp-matrix
+			        ))
+   (:cls-data      :depends-on  )
+   (:cls-dataframe :depends-on   cls-data)
+   (:cls-dataio    : )
+   (:cls-datatrans : )
 |#	     
 	                 
 	      
 
 ;;; Basics
-
-#|
-
-  This is setup in cls.asd upon ASDF-style loading.  For any other
-  system loader, we will need to ensure the same.
-
-(defpackage :lisp-stat-config
-  (:documentation "global settings and variables. Probably need a
-                   localization tool as well.")
-  (:use :common-lisp)
-  (:export *common-lisp-stat-version*
-	   *default-path* *lsos-files* *basic-files* *ls-files*
-
-	   *cls-data-dir* *cls-home-dir* *cls-examples-dir*))
-
-|#
 
 (defpackage :lisp-stat-object-system
   (:nicknames :ls-objects :lsos)
@@ -160,7 +146,8 @@
   (:use :common-lisp)
   (:shadowing-import-from :xarray slice)
   (:export
-
+   ;; must work out what is needed for stat variable metadata, support
+   ;; functions, etc
    ))
 
 ;;; cls-data... in dataframe, though.
@@ -185,9 +172,10 @@
    varlabels caselabels  nrows ncols
    dataframe-dimension dataframe-dimensons
    xref xtype xdims xdim xrank slice take carray
-
-
- ))
+ 
+   ;; support
+   make-labels
+   ))
 
 (defpackage :cls-dataio
   (:documentation "Data I/O and similar import technologies.")
@@ -443,7 +431,7 @@
   (:export
    ;; lisp-stat-config:
    *default-path* *lsos-files* *basic-files* *ls-files*
-l   *cls-home-dir* *cls-data-dir* *cls-examples-dir*  
+   *cls-home-dir* *cls-data-dir* *cls-examples-dir*  
 
    ;; lsobjects :
    defproto defproto2
@@ -538,9 +526,11 @@ l   *cls-home-dir* *cls-data-dir* *cls-examples-dir*
    dfref dfref-case dfref-var 
    consistent-dataframe-p
    dataset list-of-columns list-of-rows
-
+   make-labels
+   
    ;; listoflist
    listoflist->dataframe listoflist->array listoflist->matrix-like
+   sublists-of-same-size-p 
 
    ;; statistics.lsp  (descriptions, should probably be moved
    ;; later...?
