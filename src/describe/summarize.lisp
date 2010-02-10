0d6d96faad5c60707b1e0d6feddd2d75b423ba9d
AJ Rossini <blindglobe@gmail.com>
2010-02-10 21:58:59 +0100

    removed unused infrastructure.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- src/describe/summarize.lisp
+++ src/describe/summarize.lisp
@@ -1,25 +1,18 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2009-04-20 13:17:34 tony>
+;;; Time-stamp: <2010-02-10 21:58:13 tony>
 ;;; Creation:   <2009-03-12 17:14:56 tony>
 ;;; File:       summarize.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
-;;; Copyright:  (c)2009--, AJ Rossini.  BSD, LLGPL, or GPLv2, depending
-;;;             on how it arrives.  
+;;; Copyright:  (c)2009--, AJ Rossini.  MIT.license (see top-level directory)
 ;;; Purpose:    Summarization strategy
 
 ;;; What is this talk of 'release'? Klingons do not make software
 ;;; 'releases'.  Our software 'escapes', leaving a bloody trail of
 ;;; designers and quality assurance people in its wake.
 
+(in-package :cls-core-methods)
 
-(defparameter *cls-summary-numerical-methods*
-  '())
-
-(defparameter *cls-summary-visual-methods*
-  '())
-
-
-
-(defgeneric summarize (ds &key type io dev)
+(defgeneric summarize (ds &allow-other-keys)
   (:documentation "general approach to providing summarizes.  Tie in different approachs to hat we want to do."))
+
