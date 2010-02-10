0cf6b4386f0fd0424ae47188617e24e781586ed1
AJ Rossini <blindglobe@gmail.com>
2010-02-10 09:27:54 +0100

    hints on directory standards.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- examples/read-me.lisp
+++ examples/read-me.lisp
@@ -1,6 +1,6 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2009-07-12 07:55:04 tony>
+;;; Time-stamp: <2010-02-10 09:17:34 tony>
 ;;; Creation:   <2009-03-12 17:14:56 tony>
 ;;; File:       read-me.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
@@ -15,9 +15,7 @@
 ;;; This organization and structure is new to the 21st Century
 ;;; version..   Think, "21st Century Schizoid Man".
 
-(in-package :ls-user) ; we do this in the CLS user playground, so we
-		      ; have access to example data.
-
+(in-package :cls-examples) 
 
 ;;; Guidelines:
 
@@ -31,3 +29,5 @@
 
 ;; Consider the use of a demo or example package to avoid pollution of
 ;; the ls-user namespace to prevent the ls-user from becoming a luser.
+
+;; use the ##-activity.lisp  naming scheme to drive what we load.
