1bf9aeb8ba8b6be4f33f3eff892bce93d0f0a83a
AJ Rossini <blindglobe@gmail.com>
2012-07-01 11:16:08 +0200

    clean up historical crud, start migration to QL as a loader-loader.

--- src/template.lisp
+++ src/template.lisp
@@ -1,10 +1,10 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2009-09-24 10:34:53 tony>
-;;; Creation:   <2009-03-12 17:14:56 tony>
+;;; Time-stamp: <2012-07-01 11:15:41 tony>
+;;; Creation:   <2009 tony>
 ;;; File:       template.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
-;;; Copyright:  (c)2009--, AJ Rossini.  Currently licensed under MIT
+;;; Copyright:  (c)2012--, AJ Rossini.  Currently licensed under MIT
 ;;;             license.  See file LICENSE.mit in top-level directory
 ;;;             for information.
 ;;; Purpose:    Template header file
