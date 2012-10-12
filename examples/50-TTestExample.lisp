36a1a8ed9eda2793c41d589843cb9ee06671c0d4
AJ Rossini <blindglobe@gmail.com>
2012-10-12 08:57:08 +0200

    renamed t-test example file, where it fits in the hierarchy.  It's still BROKEN.
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- examples/50-TTestExample.lisp
+++ examples/50-TTestExample.lisp
@@ -1,11 +1,10 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2009-04-19 09:53:53 tony>
+;;; Time-stamp: <2012-10-12 08:55:28 tony>
 ;;; Creation:   <2009-04-19 09:41:09 tony>
 ;;; File:       t-test-example.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
-;;; Copyright:  (c)2009--, AJ Rossini.  BSD, MIT, LLGPL, or
-;;;             GPLv2+, or GPLv3+ depending on how it arrives.  
+;;; Copyright:  (c)2009--, AJ Rossini.  MIT
 ;;; Purpose:    Example of basic exploratory data analysis in CLS. 
 
 ;;; What is this talk of 'release'? Klingons do not make software
@@ -13,7 +12,11 @@
 ;;; designers and quality assurance people in its wake.
 
 
-(in-package :ls-user)
+;;; If we can't do a t-test, we can't do anything
+
+;;;; BROKEN!!!
+
+(in-package :cls-user)
 
 (defparameter *my-df*
   (make-dataframe
@@ -22,4 +25,3 @@
   "Initial read-in of data.")
 
 
-;; now to build a linear regression model from *my-df*
