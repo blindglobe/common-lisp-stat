9a2606d6f33f4d51bf47234622fbda2e69985d70
David Hodge <david@hodge.id.au>
2012-12-12 23:02:17 +0800

    first cut of query function. We produce a subset of the queried dataframe. Queries can be fairly arbitrary.

--- src/data/dataframe-query.lisp
+++ src/data/dataframe-query.lisp
@@ -5,7 +5,7 @@
 ;; the general idea is to take a lisp expression and rewrite any keyword fields to call an approporate accessor
 ;; so (> :id 50) ==> (> (xref df row (translate-column :id) 50)
 ;; if a keyword is not translateable, we leave it alone.
-;; this has the side effect of 
+;; this has the side effect of introducing spelling errors later when the ex
 
 
 
@@ -17,6 +17,7 @@
 	keyword)))
 
 (defun rewrite-column-keywords (df query)
+  "look for keywords, if they are matched as part of the dataframe metdata, translate to an index number."
   (typecase query
     (cons (cons (rewrite-column-keywords df (first query))
 		(rewrite-column-keywords df (rest query))))
@@ -24,15 +25,28 @@
     (t query)))
 
 (defun make-query-lambda ( df query)
-  (compile nil   `(lambda (df the-row)
-		    (declare (ignorable df the-row))
-		    ,(rewrite-column-keywords df query))))
+  (compile nil `(lambda (df the-row)
+		  (declare (ignorable df the-row))
+		  ,(rewrite-column-keywords df query))))
+
+(defun copy-df-rows (df rows)
+  (loop for obs in rows
+	collect (loop for var below (nvars df)
+		      collect (xref df obs var)) into result
+	finally (return result)))
+
 
 (defun dfquery (df query)
   (let (( query-function  (make-query-lambda df query)))
     (loop for row below (ncases df)
 	  when (funcall query-function df row)
-	    collect row)))
+	    collect row into query-rows
+	  finally (return (if query-rows
+			      (make-dataframe
+			       ( listoflist:listoflist->array (copy-df-rows df query-rows))
+			       :varlabels (varlabels df)
+			       :vartypes (vartypes df))
+			      nil )))))
 
 (defmacro test-query-lambda ( df &rest query)
   `(make-query-lambda ,df ,@query))
@@ -40,28 +54,5 @@
 (defparameter query '(and (> :ID 50) (string= :feed "casein")))
 
 
-(defmacro fn (expr) `#', (rbuild expr))
 
- (defun rbuild (expr)
-       (if (or (atom expr) (eq (car expr) 'lambda))
-            expr
-            (if (eq (car expr) 'compose)
-                 (build-compose (cdr expr))
-                 (build-call (car expr) (cdr expr)))))
-
- (defun build-call (op fns)
-       (let ((g (gensym)))
-         `(lambda (,g)
-             (,op ,@(mapcar #'(lambda (f)
-                                      `(, (rbuild f) ,g))
-                                 fns)))))
-
- (defun build-compose (fns)
-       (let ((g (gensym)))
-         `(lambda (,g)
-             , (labels ((rec (fns)
-                             (if fns
-                                  `(, (rbuild (car fns))
-                                     , (rec (cdr fns)))
-                                  g)))
-                 (rec fns)))))
+                           
