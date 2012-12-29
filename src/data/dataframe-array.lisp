42f790e9e8ac175276ed027d60aae1bb3c1afd84
David Hodge <david@hodge.id.au>
2012-12-29 21:55:39 +0800

    moving stuff out to dataframe-query mainly

--- src/data/dataframe-array.lisp
+++ src/data/dataframe-array.lisp
@@ -33,18 +33,10 @@
   based on lisp arrays.  An obvious alternative could be a
   dataframe-matrix-like which uses the lisp-matrix classes."))
 
-(defun translate-column (df column)
-  "for production use, we would wrap this in a handler to enable entry of the correct column id"
-  (cond
-    ((typep column 'keyword)
-     (let ((col (position column (varlabels df))))
-       (if col
-	   col
-	   (error "Column name misspelt: try again ~a~%" column))))
-    ((typep column 'number) column)
-    (t (error "Invalid argument passed to translate-column ~a~%" column))))
 
 
+  
+  
 (defmethod nrows ((df dataframe-array))
   "specializes on inheritance from matrix-like in lisp-matrix."
   (array-dimension (dataset df) 0))
@@ -52,6 +44,8 @@
 (defmethod ncols ((df dataframe-array))
   "specializes on inheritance from matrix-like in lisp-matrix."
   (array-dimension (dataset df) 1))
+
+
 
 (defmethod xref ((df dataframe-array) &rest subscripts)
   "Returns a scalar in array, in the same vein as aref, mref, vref, etc.
@@ -92,83 +86,6 @@ idx1/2 is row/col or case/var."
 		    (position (elt vars j) (var-labels df))))))))
 
 
-(defmethod dfhead ((df dataframe-array)
-		   &optional (rows 10))
-  (dotimes ( i rows)
-    (format t "~A: " i)
-    (dotimes ( j (ncols df))
-      (format t "~A~t" (xref df i j)) )
-    (format t "~%")))
-
-(defmethod dfgroupby ((df dataframe-array) variable)
-  "a quick hack for summarise."
-  (let ( (h (make-hash-table :test #'equal)))
-    (dotimes (i (nrows df))
-      (incf (gethash (xref df i variable) h 0)))
-    (maphash #'(lambda (k v) (format t "~a => ~a~%" k v)) h)))
-
-(defmethod dfsummarisebycategory ((df dataframe-array) category observation function)
-  "apply function to the observation in rows identifed by the category variable"
-  (let ((category (translate-column df category))
-	(observation (translate-column df observation))
-	(h (make-hash-table :test #'equal)))
-    (dotimes (i (nrows df))
-      (push i (gethash (xref df i category) h (list))))
-   (loop for k being the hash-keys in h using (hash-value rows)
-	 collect (cons k
-		       (loop for row in rows
-			     collect (xref df row observation) into g
-			     finally (return (funcall function g)))) into groups
-	 finally (return groups))) )
 
 
 
-(defun reduce-column (df function column )
-  "reduce a column of a df with function yielding a scalar"
-  (assert (and (>= column 0) (< column (ncols df))) )
-  (loop with result = (xref df 0 column)
-	for i from 1 below (nrows df) do
-	  (setf result (funcall function result (xref df i column)))
-	finally (return result)))
-
-(defun map-column (df function column)
-  (assert (and (>= column 0) (< column (ncols df))) )
-  (loop with result = (make-sequence 'vector (nrows df) )
-	for i from 1 below (nrows df) do
-	  (setf (xref result i ) (funcall function (xref df i column)))
-	finally (return result)))
-
-(defun column-type-classifier (df column)
-  "column type classifier, finds the smallest subtype that can
-  accomodate the elements of list, in the ordering fixnum < integer <
-  float < complex < t.  Rational, float (any kind) are classified as
-  double-float, and complex numbers as (complex double-float).  Meant
-  to be used by dataframe constructors so we can guess at column data types. The presence of a non numeric in a column implies the column is represented as a non numeric, as reduces and numeric maps will fail."
-
-  (case (reduce #'max (map-column df #' 
-				  (lambda (x)
-				    (typecase x
-				      (fixnum 0)
-				      (integer 1)
-				      ((or rational double-float) 2)
-				      (complex 3)
-				      (simple-array 4)
-				      (keyword 5)
-				      (t 6))) column))
-    (0 'fixnum)
-    (1 'integer)
-    (2 'double-float)
-    (3 '(complex double-float))
-    (4 'string) ;; for the moment a categorical variable
-    (5 'keyword) ;; and likewise, regarded as a categorical varial
-    (6 t))) ;; nil will end up here.
-
-(defun infer-dataframe-types (df)
-  "infer the numeric types for each column in the dataframe. note that all non numerc types are lumped into T, so further discrimination will be required."
-  (let ((column-types (loop for col  below (ncols df)
-			    collect (column-type-classifier df col))))
-    column-types))
-
-
-(defmethod dfsummary (df dataframe-array)
-  )
