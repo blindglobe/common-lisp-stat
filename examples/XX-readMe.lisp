ec55e13ad47d797d3348bc4018f432b64d0fc5d4
aj rossini <blindglobe@gmail.com>
2013-10-15 12:22:33 +0200

    template for building examples is better explained, with intentions and purposes clarifed (it serves dual use as a training area as well as a staging area for core code).
    Signed-off-by: aj rossini <blindglobe@gmail.com>

--- examples/XX-readMe.lisp
+++ examples/XX-readMe.lisp
@@ -1,33 +1,43 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2010-02-10 09:17:34 tony>
+;;; Time-stamp: <2013-10-15 12:21:22 tony>
 ;;; Creation:   <2009-03-12 17:14:56 tony>
-;;; File:       read-me.lisp
+;;; File:       XX-readMe.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
-;;; Copyright:  (c)2009--, AJ Rossini.  BSD, LLGPL, or GPLv2, depending
-;;;             on how it arrives.  
-;;; Purpose:    Template for examples.
+;;; Copyright:  (c)2009--, AJ Rossini.  MIT license.  See LICENSE.mit
+;;;             in top level directory for details.  However this
+;;;             could be relicensed once modified, as only the
+;;;             "templating" use is intended to be MIT licensed, not
+;;;             the result of using the template!
+;;; Purpose:    Template for examples, to describe how one should use
+;;;             this directory.
 
 ;;; What is this talk of 'release'? Klingons do not make software
 ;;; 'releases'.  Our software 'escapes', leaving a bloody trail of
 ;;; designers and quality assurance people in its wake.
 
-;;; This organization and structure is new to the 21st Century
-;;; version..   Think, "21st Century Schizoid Man".
-
-(in-package :cls-examples) 
+(in-package :cls-examples)
 
 ;;; Guidelines:
 
-;; use license accord to requirements (based on included packages, or 
+;; use the ##-activity.lisp  naming scheme to drive what we load.
 
-;; use DEFPARAMETER and special variables to define useful
+;; LICENSING: use license accord to requirements (based on included packages, or
+;; what the author wants).  The default licensing is MIT, we want this
+;; to be a general prototype that someone could commercialize or use
+;; in a profit-making setting.  The goal of this project (from Tony's
+;; perspective, others are free to think differently) is to ensure
+;; that better computing is possible.  And that I have a system that I
+;; like to use.
+
+;; VARIABLES AND PLACES:  use DEFPARAMETER and special variables to define useful
 ;; structures.
 
-;; Be prepared to have a clean-up function (or attach/wrap a cleanup
-;; hook.
-
-;; Consider the use of a demo or example package to avoid pollution of
+;; NAMESPACE POLLUTION AND NAME COLLISIONS: Be prepared to have a
+;; clean-up function (or attach/wrap a cleanup hook).  Better yet,
+;; consider the use of a demo or example package to avoid pollution of
 ;; the ls-user namespace to prevent the ls-user from becoming a luser.
 
-;; use the ##-activity.lisp  naming scheme to drive what we load.
+;; REFACTORING: As code in these files gets refined, it could get
+;; moved into the core system.  All methods should start here, and get
+;; refactored into the core as they become useful.
