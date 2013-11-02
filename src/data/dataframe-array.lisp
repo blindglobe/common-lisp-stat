3e27e64f74bb7370797bf1f5d32821fa2554d45e
aj rossini <blindglobe@gmail.com>
2013-11-02 10:23:14 +0100

    removed excess dfcolumn (non-accelerated version should be just in the dataframe.lisp file.
    Signed-off-by: aj rossini <blindglobe@gmail.com>

--- src/data/dataframe-array.lisp
+++ src/data/dataframe-array.lisp
@@ -1,6 +1,6 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2013-10-21 10:39:28 tony>
+;;; Time-stamp: <2013-11-01 12:36:25 tony>
 ;;; Creation:   <2009-03-12 17:14:56 tony>
 ;;; File:       dataframe-array.lisp
 ;;; Authors:    AJ Rossini <blindglobe@gmail.com>
@@ -17,7 +17,6 @@
 (in-package :cls-dataframe)
 
 ;;; DATAFRAME-ARRAY
-;;;
 
 (defclass dataframe-array (dataframe-like)
   ((store :initform nil
@@ -28,14 +27,10 @@
    (store-class :initform 'array
 		:accessor store
 		:documentation "Storage class is ARRAY."))
-  (:documentation "example implementation of dataframe-like using
-  storage based on lisp arrays.  An obvious alternative could be a
-  dataframe-matrix-like which uses the lisp-matrix classes."))
-
-
-(defmethod dfcolumn ((df dataframe-array) variable)
-  "return a column as a list. a quick hack until we decide what the array manipulations should be"
-  (loop for row below (nrows df) collect (xref df row variable)))
+  (:documentation
+   "example implementation of dataframe-like using storage based on
+    lisp arrays.  An obvious alternative could be a
+    dataframe-matrix-like which uses the lisp-matrix classes."))
 
 (defmethod make-dataframe2 ((data dataframe-array)
 			    &key vartypes varlabels caselabels doc)
