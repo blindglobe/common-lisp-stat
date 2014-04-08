a5b69722877a7f1de0583a67728cdf9138e1d32a
AJ Rossini <blindglobe@gmail.com>
2014-04-08 09:51:31 +0200

    just checkpointing the initialization of the data frame comparison experiments.
    Nothing new, move along.
    
    Signed-off-by: AJ Rossini <blindglobe@gmail.com>

--- examples/024-dataframeBuilding-CLDATAFRAME.lisp
+++ examples/024-dataframeBuilding-CLDATAFRAME.lisp
@@ -1,49 +1,45 @@
 ;;; -*- mode: lisp -*-
 
-;;; Time-stamp: <2014-04-04 15:58:33 tony>
-;;; Creation:   <2009-03-12 17:14:56 tony>
-;;; File:       024/-dataframeBuilding-CLDATAFRAME.lisp
+;;; Time-stamp: <2014-04-08 09:49:34 tony>
+;;; Creation:   <2014-04-08 09:49:19 tony>
+;;; File:       024-dataframeBuilding-CLDATAFRAME.lisp
 ;;; Author:     AJ Rossini <blindglobe@gmail.com>
 ;;; Copyright:  (c)2014--, AJ Rossini.  MIT license.  See LICENSE.mit
 ;;;             in top level directory for details.
-;;;
-;;; Purpose:    Experiments for using CL-DATA-FRAME
+;;; Purpose:    Experiments for using dataframe packages, as part of
+;;;             scope/seek phase of development.
 
 ;;; What is this talk of 'release'? Klingons do not make software
 ;;; 'releases'.  Our software 'escapes', leaving a bloody trail of
 ;;; designers and quality assurance people in its wake.
 
-(in-package :cls-examples)
+(in-package :cl-user)
 
+;;; Load and create examples packages for the package structures.
 
 (ql:quickload :cl-data-frame)
-(use-
-;;; Guidelines:
+(ql:quickload :data-table)
 
-;; use the ###-activity.lisp  naming scheme to drive what we load.
+(defpackage :cls-examples-cl-data-frame
+  ;; (:export ) nothing
+  (:use :common-lisp
+	:cl-data-frame))
 
-;; LICENSING: use license accord to requirements (based on included
-;; packages, or what the author wants).  The default licensing is MIT,
-;; we want this to be a general prototype that someone could
-;; commercialize or use in a profit-making setting.  The goal of this
-;; project (from Tony's perspective, others are free to think
-;; differently) is to ensure that better computing is possible.  And
-;; that I have a system that I like to use.
+(defpackage :cls-examples-data-table
+  ;; (:export ) nothing
+  (:use :common-lisp
+	:data-table))
 
-;; VARIABLES AND PLACES: use DEFPARAMETER and special variables to
-;; define useful structures.
 
-;; NAMESPACE POLLUTION AND NAME COLLISIONS: Be prepared to have a
-;; clean-up function (or attach/wrap a cleanup hook).  Better yet,
-;; consider the use of a demo or example package to avoid pollution of
-;; the ls-user namespace to prevent the ls-user from becoming a luser.
+;;; Now provide examples of how they could be used.
 
-;; cls-examples could be used and contaminated!
+(in-package :cls-examples-cl-data-frame)
 
-;; REFACTORING: As code in these files gets refined, it should get
-;; moved into the core system as classes and functions demonstrate
-;; usefulness.  All methods should start here, and get refactored into
-;; the core as they become useful.
-;;
-;; Sometimes, all the advice in the above can and should be violated
-;; for all the right reasons.
+
+
+
+
+(in-package :cls-examples-data-table)
+
+
+
